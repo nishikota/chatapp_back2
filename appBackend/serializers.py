0f2ddc297abf7b675868e040bfda822863f8088b
nishikota <nishi@livicon.jp>
2021-12-03 18:12:04 +0900

    トークリスト及び相手のプロフィール参照完了 スクロール機能なし

--- appBackend/serializers.py
+++ appBackend/serializers.py
@@ -4,4 +4,4 @@ from users.models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ( 'username', 'company_name', 'section_name', 'post_name')
+        fields = ( 'id','username', 'company_name', 'section_name', 'post_name')
